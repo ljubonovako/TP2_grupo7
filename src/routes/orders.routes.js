9493d014a641783b40a3c98063dc09ff7016c8d2
Ljubo Novakovich <73858208+ljubonovako@users.noreply.github.com>
2025-11-25 18:42:57 -0300

    Add exportOrders route to orders.routes.js

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { createOrder, updateStatus } from '../controllers/orders.controller.js';
+import { createOrder, updateStatus, exportOrders } from '../controllers/orders.controller.js';
 import { authenticate, authorize } from '../middlewares/authJwt.js';
 
 const r = Router();
+r.get('/export', authenticate, authorize('admin'), exportOrders);
 r.post('/', authenticate, authorize('customer'), createOrder);
 r.put('/:id/status', authenticate, authorize('restaurant','admin'), updateStatus);
 export default r;
